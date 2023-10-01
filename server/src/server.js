090920271b7938d5f335feec04b2a50e5d1ab15b
nguyen8amk1 <trungnguyen123akohshit@gmail.com>
2023-10-01 10:29:02 +0700

    client, server can communicate now

--- server/src/server.js
+++ server/src/server.js
@@ -3,9 +3,12 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import connectDB from './config/connectDB.js';
 
+
 import 'dotenv/config.js';
 
 const app = express();
+const cors = require('cors')
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
