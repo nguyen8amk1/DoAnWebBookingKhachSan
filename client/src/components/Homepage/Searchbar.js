5061039c6c5e25801250b1493dc4c31958a360b2
Clapboiz <phamconglap03@gmail.com>
2023-10-13 14:01:37 +0700

    Search UI 60%

--- client/src/components/Homepage/Searchbar.js
+++ client/src/components/Homepage/Searchbar.js
@@ -1,6 +1,26 @@
 import "./Searchbar.scss";
+import { DateRange } from "react-date-range";
+import { useState } from "react";
+import "react-date-range/dist/styles.css"; // main css file
+import "react-date-range/dist/theme/default.css"; // theme css file
+import { format } from "date-fns";
 
 const Searchbar = () => {
+  const [openDate, setOpenDate] = useState(false);
+  const [date, setDate] = useState([
+    {
+      startDate: new Date(),
+      endDate: new Date(),
+      key: "selection",
+    },
+  ]);
+
+  const [openOptions, setOpenOptions] = useState(false);
+  const [options, SetOptions] = useState({
+    adult: 1,
+    children: 0,
+    room: 1,
+  });
   return (
     <div className="search-bar">
       <div className="content">
@@ -11,18 +31,33 @@ const Searchbar = () => {
               <div className="location">Location</div>
               <img className="inner-icon" alt="" src="/vector-4855.svg" />
             </div>
-            <div className="content__item-where">Bạn muốn đến đâu</div>
+            <div className="content__item-where">Bạn muốn đi đâu?</div>
           </div>
 
-          <div className="content__item-date">
+          <div
+            onClick={() => setOpenDate(!openDate)}
+            className="content__item-date"
+          >
             <div className="content__item-date--top">
               <img className="date-icon" alt="" src="/vector1.svg" />
               <div className="date">Date</div>
               <img className="inner-icon-1" alt="" src="/vector-4855.svg" />
             </div>
             <div className="content__item-date--check">
-              Ngày nhận phòng - Ngày trả phòng
+              {`${format(date[0].startDate, "MM/dd/yyyy")} to ${format(
+                date[0].endDate,
+                "MM/dd/yyyy"
+              )}`}
             </div>
+            {openDate && (
+              <DateRange
+                editableDateInputs={true}
+                onChange={(item) => setDate([item.selection])}
+                moveRangeOnFirstSelection={false}
+                ranges={date}
+                className="date"
+              />
+            )}
           </div>
 
           <div className="content__item-guest">
@@ -31,7 +66,35 @@ const Searchbar = () => {
               <div className="guest">Guest</div>
               <img className="inner-icon-2" alt="" src="/vector-4855.svg" />
             </div>
-            <div className="content__item-quantity">Số hành khách</div>
+            <div className="content__item-quantity">
+              {`${options.adult} Người lớn · ${options.children} Trẻ em · ${options.room} Phòng`}
+              <div className="guest__options">
+                <div className="guest__options-item">
+                  <span className="guest__options-text">Người lớn</span>
+                  <div className="guest__options-counter">
+                    <button className="guest__options-counter-button">-</button>
+                    <span className="guest__options-counter-number">1</span>
+                    <button className="guest__options-counter-button">+</button>
+                  </div>
+                </div>
+                <div className="guest__options-item">
+                  <span className="guest__options-text">Trẻ em</span>
+                  <div className="guest__options-counter">
+                    <button className="guest__options-counter-button">-</button>
+                    <span className="guest__options-counter-number">0</span>
+                    <button className="guest__options-counter-button">+</button>
+                  </div>
+                </div>
+                <div className="guest__options-item">
+                  <span className="guest__options-text">Phòng</span>
+                  <div className="guest__options-counter">
+                    <button className="guest__options-counter-button">-</button>
+                    <span className="guest__options-counter-number">1</span>
+                    <button className="guest__options-counter-button">+</button>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
 
