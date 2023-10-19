99f547863f638497f0255ca7eea710e36ad73c39
Clapboiz <phamconglap03@gmail.com>
2023-10-20 01:08:53 +0700

    fix full bug sidebar static + dynamic but have bug sticky

---
+++ client/src/components/Searchpage/searchpage__searchitem.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "../../style/Searchpage/searchpage__searchitem.scss";
+
+const searchpage__searchitem = () => {
+  return <div></div>;
+};
+
+export default searchpage__searchitem;
