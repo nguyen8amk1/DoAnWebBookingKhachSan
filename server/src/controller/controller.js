519cc0cee4c5736f6ba422c9429bf89ab0ff0f12
nguyen8amk1 <trungnguyen123akohshit@gmail.com>
2023-10-31 10:26:35 +0700

    Update controller.js

--- server/src/controller/controller.js
+++ server/src/controller/controller.js
@@ -18,14 +18,28 @@ const postCrud = async (req, res) => {
     // return res.render('crud.ejs');
 }
 
+const nameMapping = (name) => {
+
+    switch(name) {
+        case "Vung Tau": 
+            return "vungtau";
+        break;
+        case "HCM": 
+            return "hcm";
+        break;
+        case "Ha Noi": 
+            return "hanoi";
+        break;
+        case "Da Nang": 
+            return "danang";
+        break;
+        case "Da Lat": 
+            return "dalat";
+        break;
+    }
+}
+
 const searchForPlaces = async (req, res) => {
-    // Return format: 
-        // Name  
-        // Price 
-        // Score 
-        // Images 
-        
-    // TODO: call the ORM to get the data needed with these parameters
     // + City 
     // @Note:The date is not working (for now :v)
     // + Date:
@@ -36,15 +50,16 @@ const searchForPlaces = async (req, res) => {
     //     + childrens
 
     // test url: 
-    // http://127.0.0.1:8080/searchforplaces?city="Vung Tau"&date_came="10/20/2023"&date_leave="10/21/2023"&members_count_adults=2&members_count_children=1
+    // http://127.0.0.1:8080/searchforplaces?city=Vung Tau&date_came=10/20/2023&date_leave=10/21/2023&members_count_adults=2&members_count_children=1
 
     console.log(req.query);
-    const queryStr = "SELECT Hotels.name, Hotels.address, Hotels.score, Hotels.images FROM `Hotels` INNER JOIN `Cities` ON Hotels.cityID=Cities.id";
-    const [results, metadata] = await sequelize.query(queryStr, {type: QueryTypes.SELECT});
+    let cityName = nameMapping(req.query.city);
+    const queryStr = "SELECT * FROM `Hotels` INNER JOIN `Cities` ON Hotels.cityID=Cities.id" + ` WHERE Cities.name='${cityName}'`;
+    const [results, metadata] = await sequelize.query(queryStr, {type: QueryTypes.RAW});
 
-    console.log(results);
+    // console.log(results);
 
-    const testData = [results]; 
+    const testData = results; 
     return res.send(testData);
 }
 
@@ -56,14 +71,13 @@ const getHotelDetails = async (req, res) => {
         // Images 
 
     // input: hotelsId 
+    // test url: http://127.0.0.1:8080/hoteldetails?hotel_id=5
     const hotelId = req.query.hotel_id; 
     console.log(hotelId);
     const queryStr = "SELECT * FROM `Hotels` WHERE Hotels.id=" + hotelId;
     const [results, metadata] = await sequelize.query(queryStr, {type: QueryTypes.SELECT});
 
-    const testData = {
-        results
-    };
+    const testData = results;
 
     return res.send(testData);
 }
