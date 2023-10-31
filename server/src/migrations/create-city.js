4ade65b90542f55aadaf06f7facd2382085d8703
nguyen8amk1 <trungnguyen123akohshit@gmail.com>
2023-10-31 10:03:41 +0700

    seeding worked

--- server/src/migrations/create-city.js
+++ server/src/migrations/create-city.js
@@ -22,6 +22,7 @@ module.exports = {
             }
         });
     },
+
     async down(queryInterface, Sequelize) {
         await queryInterface.dropTable('Cities');
     }
--- server/src/seeders/seed-room.js
+++ server/src/seeders/seed-room.js
@@ -1,4 +1,22 @@
 'use strict';
+const fs = require("fs");
+const util = require("util");
+
+const readFileAsync = util.promisify(fs.readFile);
+
+async function readJson(fileName) {
+    try {
+        // Read the specified JSON file using promisified fs.readFile.
+        const data = await readFileAsync(fileName);
+
+        // Parse the JSON data.
+        const student = JSON.parse(data);
+        // console.log(student); // Print the student data.
+        return student; // Return the student data.
+    } catch (err) {
+        throw err; // Throw any errors encountered during the process.
+    }
+}
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
@@ -13,21 +31,29 @@ module.exports = {
          *   isBetaMember: false
          * }], {});
         */
-        const roomData = [];
-        for (let i = 0; i < 25; i++) {
-            roomData.push({
-                prices: faker.number.int({ min: 1000000, max: 10000000 }).toLocaleString(),
-                status: faker.number.int({ min: 0, max: 1 }).toLocaleString(),
-                adultCount: faker.number.int({ min: 1, max: 4 }).toLocaleString(),
-                childrenCount: faker.number.int({ min: 0, max: 2 }).toLocaleString(),
-                occupationStar: faker.date.past().toLocaleDateString(),
-                occupationEnd: faker.date.future().toLocaleDateString(),
-                hotelID: faker.number.int({ min: 1, max: 25 }),
-                createdAt: new Date(),
-                updatedAt: new Date(),
-            });
+
+        try {
+            const data = await readJson("../scrapingTool/scrapedData/data.json");
+            const roomData = [];
+            for (let i = 0; i < 25; i++) {
+                roomData.push({
+                    prices: faker.number.int({ min: 1000000, max: 10000000 }).toLocaleString(),
+                    status: faker.number.int({ min: 0, max: 1 }).toLocaleString(),
+                    adultCount: faker.number.int({ min: 1, max: 4 }).toLocaleString(),
+                    childrenCount: faker.number.int({ min: 0, max: 2 }).toLocaleString(),
+                    occupationStar: faker.date.past().toLocaleDateString(),
+                    occupationEnd: faker.date.future().toLocaleDateString(),
+                    hotelID: faker.number.int({ min: 1, max: 25 }),
+                    createdAt: new Date(),
+                    updatedAt: new Date(),
+                });
+            }
+            return queryInterface.bulkInsert('Rooms', roomData);
+
+        } catch (err) {
+            console.error("An error occurred:", err);
         }
-        return queryInterface.bulkInsert('Rooms', roomData);
+
     },
 
     async down(queryInterface, Sequelize) {
