dc1d0b3bdf371a0aa6cdeaef932603d4d00513c0
nguyen8amk1 <trungnguyen123akohshit@gmail.com>
2023-11-06 23:19:00 +0700

    save point

--- server/src/controller/authController.js
+++ server/src/controller/authController.js
@@ -10,7 +10,7 @@ const refreshTokens = [];
 
 const checkUserCredential = (username, password) => {
     // TODO: Connect to database to check, but currently just have some fixed values 
-    if(!(username === "ditme" && password==='saigon')) {
+    if (!(username === "ditme" && password === 'saigon')) {
         return false;
     }
     return true;
@@ -18,23 +18,26 @@ const checkUserCredential = (username, password) => {
 
 const login = async (req, res) => {
     console.log(req.body);
-    const username = req.body.username; 
+    const username = req.body.username;
     const password = req.body.password;
     const role = "user";
     // TODO: check step: 
     const userExist = checkUserCredential(username, password);
-    if(!userExist) {
-        res.status(400).json({error: "User not exist"})
+    if (!userExist) {
+        res.status(400).json({ error: "User not exist" })
     }
     const user = {
         username: username,
         role: role// get from database  
     }
     // res.json({msg: "ditme nguoi dung co ton tai em nhe :v"});
-    const accessToken = generateAccessToken(user); 
+    const accessToken = generateAccessToken(user);
     const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET);
+
+    // NOTE: this refresh token push thing, should be push to database
     refreshTokens.push(refreshToken);
-    res.json({ accessToken: accessToken, refreshToken: refreshToken }); 
+
+    res.json({ accessToken: accessToken, refreshToken: refreshToken });
 }
 
 const register = async (req, res) => {
@@ -42,12 +45,28 @@ const register = async (req, res) => {
 
 }
 
-const logout = async (req, res) => {
+const getNewToken = async (req, res) => {
+    const refreshToken = req.body.token;
+    if (refreshToken == null) return res.sendStatus(401).json({ error: "There is no refresh token" });
+    if (!refreshTokens.includes(refreshToken)) return res.sendStatus(403).json({ error: "The refresh token does not exist"});
 
+    jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+        if (err) return res.sendStatus(403).json({error: "Refresh token does not valid"});
+        const accessToken = generateAccessToken({ name: user.name });
+        res.json({ accessToken: accessToken });
+    })
 }
 
-export default { 
+const logout = async (req, res) => {
+    // TODO: change this to delete from the database  
+    refreshTokens = refreshTokens.filter(token => token !== req.body.token);
+    res.sendStatus(204);
+}
+
+
+export default {
+    getNewToken: getNewToken,
     login: login,
-    register: register, 
+    register: register,
     logout: logout,
 }
--- server/src/route/authRoute.js
+++ server/src/route/authRoute.js
@@ -4,6 +4,7 @@ import authController   from "../controller/authController";
 const router = express.Router();
 
 const initAuthRouters = (app) => {
+    router.post('/token', authController.getNewToken);
     router.post('/login', authController.login);
     router.post('/register', authController.register);
     router.delete('/logout', authController.logout);
