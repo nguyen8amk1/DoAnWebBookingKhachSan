ec199e7ba291455fcef54bc62cc0a7bda8737769
nguyen8amk1 <trungnguyen123akohshit@gmail.com>
2023-11-09 17:37:56 +0700

    Save point to create another branch

---
+++ client/src/views/HotelDetail.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import UserLogin from './UserLogin';
+import UserRegister from './UserRegister';
+
+class UserInfoComponent extends React.Component {
+    constructor(props) { 
+
+    }
+
+    render() {
+        return <>
+
+            <UserInfoComponent />
+        </>;
+    }
+}
