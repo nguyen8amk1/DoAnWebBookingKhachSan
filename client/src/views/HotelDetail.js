67fc6a1e4c335295270bd42923a5c523f3180fba
nghiatrong016 <21522375@gm.uit.edu.vn>
2023-11-26 13:31:26 +0700

    1 more section images

--- client/src/views/HotelDetail.js
+++ client/src/views/HotelDetail.js
@@ -5,7 +5,13 @@ import SubNavMain from '../components/Homepage/SubNavMain';
 // import MultiitemCarousel from '../components/MultiitemCarousel';
 import { Link } from "react-router-dom";
 import '../style/HotelDetail.scss'
-
+import {
+    MDBCol,
+    MDBContainer,
+    MDBIcon,
+    MDBRow,
+    MDBTypography,
+} from "mdb-react-ui-kit";
 
 class HotelDetail extends React.Component {
     constructor(props) {
@@ -192,6 +198,136 @@ class HotelDetail extends React.Component {
                     src="/Destination-icon--red.svg"
                 />
                 <b>Đánh giá của khách hàng</b>
+                <MDBContainer className="Multi-container py-5">
+                    <MDBRow className="d-flex justify-content-center">
+                        <MDBCol md="10" xl="8" className="text-center">
+                            <h3 className="mb-4">Testimonials</h3>
+                            <p className="mb-4 pb-2 mb-md-5 pb-md-0">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit,
+                                error amet numquam iure provident voluptate esse quasi, veritatis
+                                totam voluptas nostrum quisquam eum porro a pariatur veniam.
+                            </p>
+                        </MDBCol>
+                    </MDBRow>
+                    <MDBRow className="text-center">
+                        <MDBCol md="4" className="mb-5 mb-md-0">
+                            <div className="d-flex justify-content-center mb-4">
+                                <img
+                                    src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(1).webp"
+                                    className="rounded-circle shadow-1-strong"
+                                    width="150"
+                                    height="150"
+                                />
+                            </div>
+                            <h5 className="mb-3">Maria Smantha</h5>
+                            <h6 className="text-primary mb-3">Web Developer</h6>
+                            <p className="px-xl-3">
+                                <MDBIcon fas icon="quote-left" className="pe-2" />
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod eos
+                                id officiis hic tenetur quae quaerat ad velit ab hic tenetur.
+                            </p>
+                            <MDBTypography
+                                listUnStyled
+                                className="d-flex justify-content-center mb-0"
+                            >
+                                <li>
+                                    <MDBIcon fas icon="star" size="sm" className="text-warning" />
+                                </li>
+                                <li>
+                                    <MDBIcon fas icon="star" size="sm" className="text-warning" />
+                                </li>
+                                <li>
+                                    <MDBIcon fas icon="star" size="sm" className="text-warning" />
+                                </li>
+                                <li>
+                                    <MDBIcon fas icon="star" size="sm" className="text-warning" />
+                                </li>
+                                <li>
+                                    <MDBIcon
+                                        fas
+                                        icon="star-half-alt"
+                                        size="sm"
+                                        className="text-warning"
+                                    />
+                                </li>
+                            </MDBTypography>
+                        </MDBCol>
+                        <MDBCol md="4" className="mb-5 mb-md-0">
+                            <div className="d-flex justify-content-center mb-4">
+                                <img
+                                    src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(2).webp"
+                                    className="rounded-circle shadow-1-strong"
+                                    width="150"
+                                    height="150"
+                                />
+                            </div>
+                            <h5 className="mb-3">Lisa Cudrow</h5>
+                            <h6 className="text-primary mb-3">Graphic Designer</h6>
+                            <p className="px-xl-3">
+                                <MDBIcon fas icon="quote-left" className="pe-2" />
+                                Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis
+                                suscipit laboriosam, nisi ut aliquid commodi.
+                            </p>
+                            <MDBTypography
+                                listUnStyled
+                                className="d-flex justify-content-center mb-0"
+                            >
+                                <li>
+                                    <MDBIcon fas icon="star" size="sm" className="text-warning" />
+                                </li>
+                                <li>
+                                    <MDBIcon fas icon="star" size="sm" className="text-warning" />
+                                </li>
+                                <li>
+                                    <MDBIcon fas icon="star" size="sm" className="text-warning" />
+                                </li>
+                                <li>
+                                    <MDBIcon fas icon="star" size="sm" className="text-warning" />
+                                </li>
+                                <li>
+                                    <MDBIcon fas icon="star" size="sm" className="text-warning" />
+                                </li>
+                            </MDBTypography>
+                        </MDBCol>
+                        <MDBCol md="4" className="mb-5 mb-md-0">
+                            <div className="d-flex justify-content-center mb-4">
+                                <img
+                                    src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(9).webp"
+                                    className="rounded-circle shadow-1-strong"
+                                    width="150"
+                                    height="150"
+                                />
+                            </div>
+                            <h5 className="mb-3">John Smith</h5>
+                            <h6 className="text-primary mb-3">Marketing Specialist</h6>
+                            <p className="px-xl-3">
+                                <MDBIcon fas icon="quote-left" className="pe-2" />
+                                At vero eos et accusamus et iusto odio dignissimos ducimus qui
+                                blanditiis praesentium voluptatum deleniti atque corrupti.
+                            </p>
+                            <MDBTypography
+                                listUnStyled
+                                className="d-flex justify-content-center mb-0"
+                            >
+                                <li>
+                                    <MDBIcon fas icon="star" size="sm" className="text-warning" />
+                                </li>
+                                <li>
+                                    <MDBIcon fas icon="star" size="sm" className="text-warning" />
+                                </li>
+                                <li>
+                                    <MDBIcon fas icon="star" size="sm" className="text-warning" />
+                                </li>
+                                <li>
+                                    <MDBIcon fas icon="star" size="sm" className="text-warning" />
+                                </li>
+                                <li>
+                                    <MDBIcon far icon="star" size="sm" className="text-warning" />
+                                </li>
+                            </MDBTypography>
+                        </MDBCol>
+                    </MDBRow>
+                </MDBContainer>
                 <div className='rating-secion'>
                     <button type="button" className="btn btn-primary">8.9</button>
                     <p className='dot-adjust'> Xuất sắc &#183; 4 đánh giá &#183; Đọc tất cả đánh giá </p>
@@ -266,7 +402,7 @@ class HotelDetail extends React.Component {
                     <div className='member-item-seventh'>
                         <div className='left-item'><i class="fa fa-credit-card" aria-hidden="true"></i>Các phương thức thanh toán được chấp nhận</div>
                         <div className='right-item'>
-                            Vật nuôi không được phép
+                            <img src="https://cf.bstatic.com/static/img/transparent/8e09e5757781bf4c0f42228d45f422e5e800ae64.gif" class="payment_methods_readability ppd_payment_methods_readability creditcard maestro" alt="Maestro" title="Maestro" />
                         </div>
                     </div>
                 </div>
